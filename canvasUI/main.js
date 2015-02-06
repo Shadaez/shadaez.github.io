c3ccf35fafe1acb912fdde6a04b1c761a9d8a0b6
Justin Wheeler <justintymewheeler@gmail.com>
2015-02-06 14:18:53 -0800

    test without round

--- canvasUI/main.js
+++ canvasUI/main.js
@@ -15,7 +15,7 @@ var canvas = document.getElementById('canvas'),
     FONTWIDTH = 11,
     PADDING = 7.5,
     TORAD = Math.PI / 180,
-    MAXSIZE = Math.round(Math.sqrt(Math.pow(canvas.height, 2) + Math.pow(canvas.width, 2)) + 1),
+    MAXSIZE = (Math.sqrt(Math.pow(canvas.height, 2) + Math.pow(canvas.width, 2)) + 1),
     rollTicks = [-60, -45, -20, -10, 10, 20, 45, 60];
     ctx.lineWidth = '2';
 
@@ -24,14 +24,14 @@ ctx.fillStyle = 'white';
 function main(){
 ctx.save();
   //CENTER
-  ctx.translate(Math.round(canvas.width/2), Math.round((canvas.height + TOPHEIGHT)/2));
+  ctx.translate((canvas.width/2), ((canvas.height + TOPHEIGHT)/2));
   ctx.save()
   //CENTER ROLLED
   ctx.rotate(TORAD * values.roll);
   ctx.strokeStyle = 'white';
   ctx.fillStyle = '#ae641b';
   ctx.beginPath();
-    ctx.rect(Math.round(-MAXSIZE/2), Math.round(-values.pitch * 4 >= MAXSIZE/2 ? -MAXSIZE/2 : values.pitch*4), Math.round(MAXSIZE), (values.pitch*4 >= MAXSIZE/2 ? 0 : 1) * (-values.pitch*4 >= MAXSIZE/2? MAXSIZE : -values.pitch*4 + MAXSIZE/2));
+    ctx.rect((-MAXSIZE/2), (-values.pitch * 4 >= MAXSIZE/2 ? -MAXSIZE/2 : values.pitch*4), (MAXSIZE), (values.pitch*4 >= MAXSIZE/2 ? 0 : 1) * (-values.pitch*4 >= MAXSIZE/2? MAXSIZE : -values.pitch*4 + MAXSIZE/2));
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
@@ -44,18 +44,18 @@ ctx.save();
   ctx.stroke();
   ctx.strokeStyle = 'white';
   ctx.beginPath();
-  ctx.rect(Math.round(-canvas.width/4), Math.round(-canvas.height/4), Math.round(canvas.width/2), Math.round(canvas.height/2));
+  ctx.rect((-canvas.width/4), (-canvas.height/4), (canvas.width/2), (canvas.height/2));
   ctx.closePath();
   ctx.clip();
   ctx.beginPath();
   for(i = 0; i < 12; i++){
-    xCache = Math.round(mod(values.pitch * 4 + ((i-6) * 20), 40) < 20 ? 50 : 25);
-    yCache = Math.round(20 * (i - 6) + mod(values.pitch * 4, 20));
+    xCache = (mod(values.pitch * 4 + ((i-6) * 20), 40) < 20 ? 50 : 25);
+    yCache = (20 * (i - 6) + mod(values.pitch * 4, 20));
     if(xCache != 25){
       textCache = (Math.floor((values.pitch + 5 * (6 - i)) / 10) * 10).toString();
       textCache = textCache === '0' ? '' : textCache;
-      ctx.fillText(textCache, xCache, Math.round(yCache + FONTHEIGHT/4));
-      ctx.fillText(textCache, Math.round(-xCache - (textCache.length * FONTWIDTH) - 3), Math.round(yCache + FONTHEIGHT/4));
+      ctx.fillText(textCache, xCache, (yCache + FONTHEIGHT/4));
+      ctx.fillText(textCache, (-xCache - (textCache.length * FONTWIDTH) - 3), (yCache + FONTHEIGHT/4));
     }
     ctx.moveTo(-xCache, yCache);
     ctx.lineTo(xCache , yCache);
@@ -90,7 +90,7 @@ ctx.save();
   ctx.strokeStyle = 'white';
   ctx.lineWidth = '2';
   ctx.beginPath();
-    ctx.rect(0, 0, canvas.width, TOPHEIGHT + Math.round(ctx.lineWidth/2));
+    ctx.rect(0, 0, canvas.width, TOPHEIGHT + (ctx.lineWidth/2));
   ctx.closePath();
   ctx.fill();
   ctx.save();
@@ -113,16 +113,16 @@ ctx.save();
   ctx.stroke();
   ctx.clip();
   ctx.fillStyle = 'white';
-  ctx.translate(PADDING + BARWIDTH, Math.round((canvas.height + TOPHEIGHT)/2));
+  ctx.translate(PADDING + BARWIDTH, ((canvas.height + TOPHEIGHT)/2));
   ctx.beginPath();
   for(i = 0; i < 16; i++){
-      yCache = Math.round(25 * (i - 8) + mod(values.speed * 5, 25));
-      xCache = Math.round(mod(values.speed * 5 + (25 * (i - 8)), 50) < 25 ? -12 : -6);
+      yCache = (25 * (i - 8) + mod(values.speed * 5, 25));
+      xCache = (mod(values.speed * 5 + (25 * (i - 8)), 50) < 25 ? -12 : -6);
       ctx.moveTo(xCache, yCache);
       ctx.lineTo(0, yCache);
       if(xCache != -6){
         textCache = (Math.floor((values.speed + 5 * (8 - i)) / 10) * 10).toString();
-        ctx.fillText(textCache, 0 - 12 - 3 - textCache.length * FONTWIDTH, Math.round(yCache + FONTHEIGHT/4));
+        ctx.fillText(textCache, 0 - 12 - 3 - textCache.length * FONTWIDTH, (yCache + FONTHEIGHT/4));
       }
   }
   ctx.closePath();
@@ -134,8 +134,8 @@ ctx.save();
   ctx.fill();
   ctx.stroke();
   ctx.fillStyle = 'white';
-  textCache = Math.round(values.speed).toString();
-  ctx.fillText(textCache, -FONTWIDTH * textCache.length - 12 - 3, Math.round(FONTHEIGHT/4 + 2));
+  textCache = (values.speed).toString();
+  ctx.fillText(textCache, -FONTWIDTH * textCache.length - 12 - 3, (FONTHEIGHT/4 + 2));
   ctx.restore();
   ctx.save();
   ctx.beginPath();
@@ -156,17 +156,17 @@ ctx.save();
   ctx.globalAlpha = '1';
   ctx.stroke();
   ctx.clip();
-  ctx.translate(canvas.width - PADDING - BARWIDTH, Math.round((canvas.height + TOPHEIGHT)/2));
+  ctx.translate(canvas.width - PADDING - BARWIDTH, ((canvas.height + TOPHEIGHT)/2));
   ctx.fillStyle = 'white';
   ctx.beginPath();
   for(i = 0; i < 16; i++){
-    yCache = Math.round(25 * (i - 8) + mod(values.altitude / 4, 25));
+    yCache = (25 * (i - 8) + mod(values.altitude / 4, 25));
     xCache = mod(Math.floor(values.altitude / 4) + (25 * (i - 8)), 50) >= 25 ? 6 : 12;
     ctx.moveTo(xCache, yCache);
     ctx.lineTo(0, yCache);
     if(xCache != 6){
       textCache = (Math.floor((values.altitude - mod(values.altitude / 4, 25) / 25 * 50 + 100 * (8 - i)) / 200) * 200);
-      ctx.fillText(textCache, 0 + 12 + 3, Math.round(yCache + FONTHEIGHT/4));
+      ctx.fillText(textCache, 0 + 12 + 3, (yCache + FONTHEIGHT/4));
     }
   }
   ctx.closePath();
@@ -178,30 +178,30 @@ ctx.save();
   ctx.fill();
   ctx.stroke();
   ctx.fillStyle = 'white';
-  ctx.fillText(Math.round(values.altitude), 12 + 3 + FONTWIDTH * 5 - FONTWIDTH * Math.round(values.altitude).toString().length, Math.round(FONTHEIGHT/4 + 2));
+  ctx.fillText((values.altitude), 12 + 3 + FONTWIDTH * 5 - FONTWIDTH * (values.altitude).toString().length, (FONTHEIGHT/4 + 2));
   ctx.fillStyle = 'black';
   ctx.beginPath();
-    ctx.rect(12 + 3 + FONTWIDTH * 2, Math.round(-FONTHEIGHT/2), Math.round(FONTWIDTH * 3.5), FONTHEIGHT);
+    ctx.rect(12 + 3 + FONTWIDTH * 2, (-FONTHEIGHT/2), (FONTWIDTH * 3.5), FONTHEIGHT);
   ctx.closePath();
   ctx.fill();
   ctx.clip();
   ctx.fillStyle = 'white';
-  ctx.font = Math.round(FONTHEIGHT * 0.75) + ' ' + ctx.font.split(' ')[1];
-  ctx.fillText(Math.round(values.altitude).toString().split('')[Math.round(values.altitude).toString().length - 3] || '', Math.round(12 + 4 + FONTWIDTH * 2.1), Math.round(FONTHEIGHT*0.75/4 + 2));
-  ctx.font = Math.round(FONTHEIGHT * 1.1) + ' ' + ctx.font.split(' ')[1];
-  ctx.fillText('00', 12 + 3 + FONTWIDTH * 3 , Math.round(FONTHEIGHT/4 + 2 + -40 + values.altitude / (50/20)%40));
-  ctx.fillText('50', 12 + 3 + FONTWIDTH * 3 , Math.round(FONTHEIGHT/4 + 2 + -20 + values.altitude / (50/20)%40));
-  ctx.fillText('00', 12 + 3 + FONTWIDTH * 3 , Math.round(FONTHEIGHT/4 + 2 + 0  + values.altitude / (50/20)%40));
-  ctx.fillText('50', 12 + 3 + FONTWIDTH * 3 , Math.round(FONTHEIGHT/4 + 2 + 20 + values.altitude / (50/20)%40));
-  ctx.fillText('00', 12 + 3 + FONTWIDTH * 3 , Math.round(FONTHEIGHT/4 + 2 + 40 + values.altitude / (50/20)%40));
+  ctx.font = (FONTHEIGHT * 0.75) + ' ' + ctx.font.split(' ')[1];
+  ctx.fillText((values.altitude).toString().split('')[(values.altitude).toString().length - 3] || '', (12 + 4 + FONTWIDTH * 2.1), (FONTHEIGHT*0.75/4 + 2));
+  ctx.font = (FONTHEIGHT * 1.1) + ' ' + ctx.font.split(' ')[1];
+  ctx.fillText('00', 12 + 3 + FONTWIDTH * 3 , (FONTHEIGHT/4 + 2 + -40 + values.altitude / (50/20)%40));
+  ctx.fillText('50', 12 + 3 + FONTWIDTH * 3 , (FONTHEIGHT/4 + 2 + -20 + values.altitude / (50/20)%40));
+  ctx.fillText('00', 12 + 3 + FONTWIDTH * 3 , (FONTHEIGHT/4 + 2 + 0  + values.altitude / (50/20)%40));
+  ctx.fillText('50', 12 + 3 + FONTWIDTH * 3 , (FONTHEIGHT/4 + 2 + 20 + values.altitude / (50/20)%40));
+  ctx.fillText('00', 12 + 3 + FONTWIDTH * 3 , (FONTHEIGHT/4 + 2 + 40 + values.altitude / (50/20)%40));
   ctx.restore();
   for(i = 0; i < 5; i++){
     ctx.beginPath();
-      ctx.rect(Math.round(canvas.width/5 * i + canvas.width/5/2 - BARWIDTH/2), 0, BARWIDTH, TOPHEIGHT);
+      ctx.rect((canvas.width/5 * i + canvas.width/5/2 - BARWIDTH/2), 0, BARWIDTH, TOPHEIGHT);
     ctx.closePath();
     ctx.stroke();
     ctx.fillStyle = 'white';
-    ctx.fillText(values.users[4+i], Math.round(canvas.width/5 * i + canvas.width/5/2 - BARWIDTH/2 + 3), FONTHEIGHT + 3);
+    ctx.fillText(values.users[4+i], (canvas.width/5 * i + canvas.width/5/2 - BARWIDTH/2 + 3), FONTHEIGHT + 3);
   }
   ctx.fillText(values.users[2], PADDING + 3, TOPHEIGHT + PADDING + FONTHEIGHT + 3);
   ctx.fillText(values.users[3], canvas.width - PADDING - BARWIDTH + 3, TOPHEIGHT + PADDING + FONTHEIGHT + 3);
